82c798956eb432c1a508464561f97b1df8117203
James Maxwell <10james.maxwell@gmail.com>
2019-12-17 18:12:25 +0000

    mem leaks

---
+++ src/cmake-debug-linux/CMakeFiles/treecode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/treecode.dir/pyInterface.cpp.o"
+  "treecode.cpython-36m-x86_64-linux-gnu.so"
+  "treecode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/treecode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/cmake-debug-linux/CMakeFiles/vecMaths.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vecMaths.dir/vecMaths.cpp.o"
+  "libvecMaths.a"
+  "libvecMaths.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vecMaths.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
